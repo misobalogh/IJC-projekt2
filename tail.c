678c9083163110dacb79e927479b4911d3733281
Michal Balogh <baloghmichal03@gmail.com>
2023-03-25 15:00:56 +0100

    implemented cb structs and create and free functions

--- tail.c
+++ tail.c
@@ -19,38 +19,67 @@
 #define MAX_LINE_SIZE 4096
 #define DEFAULT_LAST_N 10
 
-typedef struct 
+typedef struct
 {
     long number_of_lines;
-    FILE* fp;
-} args_t;
+    FILE *fp;
 
+} args_t;
 
 typedef struct
 {
     unsigned int size; // max size 4095
     char line[MAX_LINE_SIZE];
+
 } cb_element;
+
+typedef struct
+{
+    unsigned int start;
+    unsigned int end;
+    cb_element *element;
+    unsigned int size;
+
+} cb_t;
 
 //=============================== Interface ===============================//
 
 args_t parse_args(int argc, char **argv);
 
-// void cb_create(n);
+cb_t *cb_create(unsigned long n);
 
 // void cb_put(cb,line);
 
 // cb_element cb_get(cb);
 
-// void cb_free(cb);
+void cb_free(cb_t *cb);
 
 //=============================== Functions ===============================//
 
+cb_t *cb_create(unsigned long n)
+{
+    cb_t *cb = malloc(sizeof(cb_t));
+    cb->size = n;
+    cb->start = 0;
+    cb->end = 0;
+    cb->element = malloc(sizeof(cb_element) * cb->size);
+    return cb;
+}
+
+void cb_free(cb_t *cb)
+{
+    long size = cb->size;
+    for(int i = 0; i < size; i++)
+    {
+        free(cb->element[i].line);
+    }
+    free(cb);
+}
 
 args_t parse_args(int argc, char **argv)
 {
     args_t args;
-    args.fp = stdin;                  // default input file
+    args.fp = stdin;                   // default input file
     const char *program_switch = "-n"; // program optional switch
     long last_n_lines = DEFAULT_LAST_N;
 
@@ -84,7 +113,7 @@ args_t parse_args(int argc, char **argv)
     {
         args.fp = fopen(argv[1], "r");
     }
-   
+
     return args;
 
 error_exit1:
@@ -96,7 +125,7 @@ error_exit1:
 
 int main(int argc, char **argv)
 {
-    args_t args = parse_args(argc, argv);
+    /*args_t args = parse_args(argc, argv);
     if (args.fp == NULL)
     {
         fprintf(stderr, "Error: Could not open file\n");
@@ -104,10 +133,18 @@ int main(int argc, char **argv)
     }
 
     char c;
-    while ((c = fgetc(args.fp)) != EOF){
+    while ((c = fgetc(args.fp)) != EOF)
+    {
         putchar(c);
     }
     putchar('\n');
 
-    fclose(args.fp);
+    fclose(args.fp);*/
+
+    cb_t *cbufik = cb_create(10);
+    strcpy(cbufik->element->line,"string");
+
+    printf("%s\n", cbufik->element->line);
+
+    cb_free(cbufik);
 }
